fadef7c47ec366e7f7edf170c60fa1f1b3f7d522
NullerGoej <bastian2010@live.dk>
2023-04-14 10:40:19 +0200

    Match

--- src/app/match-profile/match-profile.component.ts
+++ src/app/match-profile/match-profile.component.ts
@@ -1,10 +1,12 @@
 import { Component, Input } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-match-profile',
   templateUrl: './match-profile.component.html',
   styleUrls: ['./match-profile.component.scss'],
   standalone: true,
+  imports: [IonicModule],
 })
 export class MatchProfileComponent {
 
