0150de1affd1164944cb43f063803c814037dff8
jefferen <jeff0167@edu.zealand.dk>
2023-04-16 14:57:48 +0200

    Back button

--- src/app/tab3/chat/chat.page.spec.ts
+++ src/app/tab3/chat/chat.page.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { IonicModule } from '@ionic/angular';
 import { ChatPage } from './chat.page';
 
 describe('ChatPage', () => {
