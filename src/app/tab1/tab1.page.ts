f1ab90b5c7b849ad5b2f4b2364ef1797fab039d2
jefferen <jeff0167@edu.zealand.dk>
2023-04-16 11:26:48 +0200

    Tab1 routing, editting setup

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,14 +1,23 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { CommonModule } from '@angular/common';
+import { ProfileService } from '../services/profile.service';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
   standalone: true,
-  imports: [IonicModule, ExploreContainerComponent],
+  imports: [IonicModule, ExploreContainerComponent, CommonModule, RouterModule],
 })
 export class Tab1Page {
-  constructor() {}
+
+  loggedInPerson: any;
+
+  constructor(public profileService: ProfileService) {
+    this.loggedInPerson = this.profileService.getMyProfile(); // if you start on this page you won't get you profile data
+  }
+
 }
